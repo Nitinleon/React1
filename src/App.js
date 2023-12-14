ece8e409ada350891d6c497534df948ed26fa2e4
Nitin <117649139+Nitinleon@users.noreply.github.com>
2023-12-14 11:24:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/NavComponents';
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
